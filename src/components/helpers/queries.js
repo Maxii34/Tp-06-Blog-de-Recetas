9a8f7f4d285b97c15048742111c2aa12e43d4821
Maxi Ordoñez <exemaxi@outlook.com>
2025-11-07 23:03:46 -0300

    Se agregaron las solicitudes de obtenerRecetas y borrarRecetas.

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -44,3 +44,27 @@ export const editarRecetaId = async (id, receta)=>{
         return null
     }
 }
+
+export const obtenerRecetaID= async (id)=>{
+    try {
+        const respuesta = await fetch(`${recetasBackend}/${id}`)
+        console.log(respuesta)
+        return respuesta
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
+
+export const borrarReceta= async (id)=>{
+    try {
+        const respuesta = await fetch(`${recetasBackend}/${id}`, {
+            method: 'DELETE'
+        })
+        console.log(respuesta)
+        return respuesta
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
